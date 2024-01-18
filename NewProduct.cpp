99cdab7cf6b4509c424462c414d6a423603ff34c
Chanrit-Chi <chanrith.chi@gmail.com>
2024-01-18 13:34:16 +0700

    1

---
+++ NewProduct.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Product.cpp"
+
+using namespace std;
+
+class NewProduct : public Product
+{
+private:
+    string ManufacturingDate;
+    int WarrantyPeriod;
+};
